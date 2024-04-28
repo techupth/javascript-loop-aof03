571245d874d2a8a7d66b69721d3b5583966407e4
kaweephat <k.kaweephat@gmail.com.com>
2024-04-28 17:27:09 +0700

    assignment-loop-fe01-aOf03

--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 let companyName = "TechUp";
 
 // Start coding here
+for (let i = 0; i < companyName.length; i++) {
+    let order = i + 1;
+    console.log("Number " + order + " character is " + companyName[i]);
+}
